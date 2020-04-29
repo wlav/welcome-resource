be006207b4d622781a0ac118d059429f76695d17
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-04-29 06:48:22 -0700

    implementation of "Hello, World!" workflow

---
+++ src/python/orquestra/welcome.py
@@ -0,0 +1,16 @@
+"""
+This module saves a welcome message.
+"""
+
+import json
+
+def welcome():
+    message = "Welcome to Orquestra!"
+
+    message_dict = {}
+    message_dict["message"] = message
+    message_dict["schema"] = "message"
+
+  # Write message to file as this will serve as output artifact
+    with open("welcome.json",'w') as f:
+        f.write(json.dumps(message_dict, indent=2))
